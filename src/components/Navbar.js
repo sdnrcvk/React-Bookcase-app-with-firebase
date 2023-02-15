00b1f2449caec95b93237fbbab8d17bf95ce6cea
Sedanur Çevik <sdnrcvk@gmail.com>
2023-02-16 01:33:54 +0300

    logout process added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
 
 export default function Navbar() {
-    
+
+  const {logout}=useLogout();
+
   return (
     <nav>
         <h1>SCapp Kitaplık</h1>
@@ -10,7 +13,7 @@ export default function Navbar() {
             <li><Link to="/">Ana Sayfa</Link></li>
             <li><Link to="/login">Giriş</Link></li>
             <li><Link to="/signup">Üye Ol</Link></li>
-            <li>Çıkış</li>
+            <li><Link to="#" onClick={logout}>Çıkış</Link></li>
         </ul>
     </nav>
   )
