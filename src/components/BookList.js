9cded3539473ac8894b2675662ca9706cea8f131
Sedanur Çevik <sdnrcvk@gmail.com>
2023-02-15 17:17:25 +0300

    deleting doc process completed

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,16 +1,24 @@
 import React from 'react'
- 
+import { db } from '../firebase/config'
+import { doc, deleteDoc } from 'firebase/firestore'
+
 export default function BookList({books}) {
 
   const handleClick=async(id)=>{
-    console.log(id);
+    //console.log(id);
+
+    const ref=doc(db,"books",id)
+
+    await deleteDoc(ref);
   }
 
   return (
     <div className='book-list'>
         <ul>
             {books.map(book=>(
-                <li key={book.id} onClick={()=>handleClick(book.id)}>{book.title}</li>
+                <li key={book.id}>{book.title}
+                <button onClick={()=>handleClick(book.id)} style={{float:"right"}}>Sil</button>
+                </li>
             ))}
         </ul>
     </div>
