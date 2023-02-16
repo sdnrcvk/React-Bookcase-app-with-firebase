16db39c5f2c4609a4bcd62f252bb1d331c4c19e4
Sedanur Çevik <sdnrcvk@gmail.com>
2023-02-16 03:13:20 +0300

    route guard process completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 function App() {
+  const {user,authIsReady}=useAuthContext();
+
   return (
     <div className="App">
-      <BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
         <Navbar/>
         <Switch>
           <Route exact path="/">
-            <Home/>
+            {user && <Home/>}
+            {!user && <Redirect to="/login"/>}
           </Route>
           <Route path="/signup">
-            <Signup/>
+            {!user && <Signup/>}
+            {user && <Redirect to="/"/>}
           </Route>
           <Route path="/login">
-            <Login/>
+            {!user && <Login/>}
+            {user && <Redirect to="/"/>}            
           </Route>
         </Switch>
       </BrowserRouter>
+      )}
     </div>
   );
 }
